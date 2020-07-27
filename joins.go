001deca4b6c17bd3672abda9fd3f873e2a728028
t.chavakis <tomchavakis@gmail.com>
2020-07-28 00:08:00 +0300

    Add MidPoint and Destination Point

---
+++ joins.go
@@ -0,0 +1,5 @@
+// Inside takes a point and a polygon and determines if the point resides inside
+// the polygon. The polygon can be convex or concave. 
+func Inside(p Point, poly Polygon) bool{
+
+}
--- measurement.go
+++ measurement.go
@@ -2,7 +2,7 @@ package turf
 
 import "math"
 
-// Distance calculates the distance between two points in degrees, radians, miles or kilometers. This uses the Haversine formula
+// Distance calculates the distance between two points in kilometers. This uses the Haversine formula
 func Distance(lon1 float64, lat1 float64, lon2 float64, lat2 float64) float64 {
 
 	dLat := DegreesToRadians(lat2 - lat1)
@@ -16,7 +16,62 @@ func Distance(lon1 float64, lat1 float64, lon2 float64, lat2 float64) float64 {
 	return d
 }
 
-// DistancePoint calculates the distance between two points
-func DistancePoint(p1 Point, p2 Point) float64 {
+// PointDistance calculates the distance between two points
+func PointDistance(p1 Point, p2 Point) float64 {
 	return Distance(p1.Lng, p1.Lat, p2.Lng, p2.Lat)
+}
+
+// Bearing finds the geographic bearing between two given points.
+func Bearing(lon1 float64, lat1 float64, lon2 float64, lat2 float64) float64 {
+	dLng := DegreesToRadians(lon2 - lon1)
+	lat1R := DegreesToRadians(lat1)
+	lat2R := DegreesToRadians(lat2)
+	y := math.Sin(dLng) * math.Cos(lat2R)
+	x := math.Cos(lat1R)*math.Sin(lat2R) - math.Sin(lat1R)*math.Cos(lat2R)*math.Cos(dLng)
+
+	// convert to degrees
+	bd := RadiansToDegrees(math.Atan2(y, x))
+
+	if bd < 0.0 {
+		bd += 360.0
+	}
+
+	if bd >= 360.0 {
+		bd -= 360.0
+
+	}
+	return bd
+
+}
+
+// PointBearing finds the geographic bearing between two points.
+func PointBearing(p1 Point, p2 Point) float64 {
+	return Bearing(p1.Lng, p1.Lat, p2.Lng, p2.Lat)
+}
+
+// MidPoint finds the point midway between them.
+func MidPoint(p1 Point, p2 Point) Point {
+	dLon := DegreesToRadians(p2.Lng - p1.Lng)
+	lat1R := DegreesToRadians(p1.Lat)
+	lon1R := DegreesToRadians(p1.Lng)
+	lat2R := DegreesToRadians(p2.Lat)
+	Bx := math.Cos(lat2R) * math.Cos(dLon)
+	By := math.Cos(lat2R) * math.Sin(dLon)
+	midLat := math.Atan2(math.Sin(lat1R)+math.Sin(lat2R), math.Sqrt((math.Cos(lat1R)+Bx)*(math.Cos(lat1R)+Bx)+By*By))
+	midLng := lon1R + math.Atan2(By, math.Cos(lat1R)+Bx)
+
+	return Point{Lat: RadiansToDegrees(midLat), Lng: RadiansToDegrees(midLng)}
+}
+
+// X --> Lon
+// Y --> Lat
+// Destination returns a destination point according to a reference point, a distance in km and a bearing in degrees from True North.
+func Destination(p1 Point, distance float64, bearing float64) Point {
+	lonR := DegreesToRadians(p1.Lng)
+	latR := DegreesToRadians(p1.Lat)
+	bR := DegreesToRadians(bearing)
+	dLat := math.Asin(math.Sin(latR)*math.Cos(distance/EarthRadius) + math.Cos(latR)*math.Sin(distance/EarthRadius)*math.Cos(bR))
+	dLng := lonR + math.Atan2(math.Sin(bR)*math.Sin(distance/EarthRadius)*math.Cos(latR), math.Cos(distance/EarthRadius)-math.Sin(latR)*math.Sin(dLat))
+
+	return Point{Lat: RadiansToDegrees(dLat), Lng: RadiansToDegrees(dLng)}
 }
