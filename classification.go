b135b6083d8cb64f4068c956525952a8140b9e7b
Thomas Chavakis <tomchavakis@gmail.com>
2020-09-06 17:31:48 +0300

    fix issues with lib

--- classification.go
+++ classification.go
@@ -1,9 +1,13 @@
 package turf
 
-import "math"
+import (
+	"math"
+
+	"github.com/tomchavakis/turf-go/geometry"
+)
 
 // NearestPoint takes a reference point and a list of points and returns the point from the point list closest to the reference.
-func NearestPoint(refPoint Point, points []Point) Point {
+func NearestPoint(refPoint geometry.Point, points []geometry.Point) geometry.Point {
 	if len(points) == 0 {
 		return refPoint
 	}
@@ -12,7 +16,7 @@ func NearestPoint(refPoint Point, points []Point) Point {
 	minDist := math.MaxFloat64
 
 	for _, point := range points {
-		dist := DistancePoint(refPoint, point)
+		dist := PointDistance(refPoint, point)
 		if dist < minDist {
 			result = point
 			minDist = dist
