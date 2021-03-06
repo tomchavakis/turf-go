24d33500349edf8202010048f61959695a5d3259
Thomas Chavakis <tomchavakis@gmail.com>
2021-03-06 18:34:42 +0200

    remove testify library (#22)

--- meta/coordAll/coordAll_test.go
+++ meta/coordAll/coordAll_test.go
@@ -1,23 +1,28 @@
 package meta
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/tomchavakis/turf-go/assert"
 	"github.com/tomchavakis/turf-go/geojson/feature"
 	"github.com/tomchavakis/turf-go/geojson/geometry"
+	"testing"
 )
 
 func TestCoordAllPont(t *testing.T) {
 	json := "{ \"type\": \"Point\", \"coordinates\": [23.0, 54.0]}"
 	geometry, err := geometry.FromJSON(json)
-	assert.Nil(t, err, "geometry can't be nil")
+	if err != nil {
+		t.Errorf("geometry error %v", err)
+	}
 
 	pt, err := geometry.ToPoint()
-	assert.Nil(t, err, "convert to Point error")
+	if err != nil {
+		t.Errorf("convert to Point error %v", err)
+	}
 
 	pts, err := CoordAll(pt, nil)
-	assert.Nil(t, err, "coord all err")
+	if err != nil {
+		t.Errorf("CoordAll err %v", err)
+	}
 
 	assert.Equal(t, len(pts), 1)
 	assert.Equal(t, pts[0].Lat, 54.0)
@@ -27,14 +32,21 @@ func TestCoordAllPont(t *testing.T) {
 func TestCoordAllLineString(t *testing.T) {
 	json := "{ \"type\": \"LineString\", \"coordinates\": [[0.0, 0.0], [1.0, 1.0]]}"
 	geometry, err := geometry.FromJSON(json)
-	assert.Nil(t, err, "geometry can't be nil")
+	if err != nil {
+		t.Errorf("geometry error %v", err)
+	}
 
 	ln, err := geometry.ToLineString()
-	assert.Nil(t, err, "convert to Linestring error")
+	if err != nil {
+		t.Errorf("convert to LineString error %v", err)
+	}
 
 	pts, err := CoordAll(ln, nil)
-	assert.Nil(t, err, "coord all err")
+	if err != nil {
+		t.Errorf("CoordAll err %v", err)
+	}
 	assert.Equal(t, len(pts), 2)
+
 	assert.Equal(t, pts[0].Lat, 0.0)
 	assert.Equal(t, pts[0].Lng, 0.0)
 
@@ -45,15 +57,20 @@ func TestCoordAllLineString(t *testing.T) {
 func TestCoordAllPolygon(t *testing.T) {
 	json := "{ \"type\": \"Polygon\", \"coordinates\": [[[0.0, 0.0], [1.0, 1.0], [0.0, 1.0], [0.0, 0.0]]]}"
 	geometry, err := geometry.FromJSON(json)
-	assert.Nil(t, err, "geometry can't be nil")
+	if err != nil {
+		t.Errorf("geometry error %v", err)
+	}
 
 	poly, err := geometry.ToPolygon()
-	assert.Nil(t, err, "convert to Polygon error")
+	if err != nil {
+		t.Errorf("convert to Polygon error %v", err)
+	}
 
 	f := false
 	pts, err := CoordAll(poly, &f)
-	assert.Nil(t, err, "coord all err")
-
+	if err != nil {
+		t.Errorf("CoordAll err %v", err)
+	}
 	assert.Equal(t, len(pts), 4)
 
 	assert.Equal(t, pts[0].Lat, 0.0)
@@ -72,14 +89,20 @@ func TestCoordAllPolygon(t *testing.T) {
 func TestCoordExclueWrapCoord(t *testing.T) {
 	json := "{ \"type\": \"Polygon\", \"coordinates\": [[[0.0, 0.0], [1.0, 1.0], [0.0, 1.0], [0.0, 0.0]]]}"
 	geometry, err := geometry.FromJSON(json)
-	assert.Nil(t, err, "geometry can't be nil")
+	if err != nil {
+		t.Errorf("geometry error %v", err)
+	}
 
 	poly, err := geometry.ToPolygon()
-	assert.Nil(t, err, "convert to Polygon error")
+	if err != nil {
+		t.Errorf("convert to Polygon error %v", err)
+	}
 
 	f := true
 	pts, err := CoordAll(poly, &f)
-	assert.Nil(t, err, "coord all err")
+	if err != nil {
+		t.Errorf("CoordAll err %v", err)
+	}
 
 	assert.Equal(t, len(pts), 3)
 
@@ -96,14 +119,20 @@ func TestCoordExclueWrapCoord(t *testing.T) {
 func TestCoordMultiPolygon(t *testing.T) {
 	json := "{ \"type\": \"MultiPolygon\", \"coordinates\": [[[[0, 0], [1, 1], [0, 1], [0, 0]]]]}"
 	geometry, err := geometry.FromJSON(json)
-	assert.Nil(t, err, "geometry can't be nil")
+	if err != nil {
+		t.Errorf("geometry error %v", err)
+	}
 
 	multiPoly, err := geometry.ToMultiPolygon()
-	assert.Nil(t, err, "convert to MultiPolygon error")
+	if err != nil {
+		t.Errorf("convert to MultiPolygon error %v", err)
+	}
 
 	f := false
 	pts, err := CoordAll(multiPoly, &f)
-	assert.Nil(t, err, "coord all err")
+	if err != nil {
+		t.Errorf("CoordAll err %v", err)
+	}
 
 	assert.Equal(t, len(pts), 4)
 
@@ -118,16 +147,19 @@ func TestCoordMultiPolygon(t *testing.T) {
 
 	assert.Equal(t, pts[3].Lat, 0.0)
 	assert.Equal(t, pts[3].Lng, 0.0)
-
 }
 
 func TestInvariantGetCoord(t *testing.T) {
 	json := "{ \"type\": \"Feature\", \"geometry\": { \"type\":\"Point\", \"coordinates\":[1,2]}}"
 	feature, err := feature.FromJSON(json)
-	assert.Nil(t, err, "feature can't be nil")
+	if err != nil {
+		t.Errorf("FromJSON error %v", err)
+	}
 
 	g, err := GetCoord(*feature)
-	assert.Nil(t, err, "feature can't be nil")
+	if err != nil {
+		t.Errorf("GetCoord error %v", err)
+	}
 
 	assert.Equal(t, g.Lat, 2.0)
 	assert.Equal(t, g.Lng, 1.0)
@@ -136,14 +168,21 @@ func TestInvariantGetCoord(t *testing.T) {
 func TestCoordAllFeatureCollection(t *testing.T) {
 	json := "{\"type\": \"FeatureCollection\", \"features\": [{\"type\": \"Feature\",\"properties\": {\"population\": 200},\"geometry\": {\"type\": \"Point\",\"coordinates\": [-112.0372, 46.608058]}}]}"
 	c, err := feature.CollectionFromJSON(json)
-	assert.Nil(t, err, "point can't be nil")
-	assert.NotNil(t, c, "feature collection can't be nil")
+	if err != nil {
+		t.Errorf("CollectionFromJSON error %v", err)
+	}
+
+	if c == nil {
+		t.Error("feature collection can't be nil")
+	}
+
 	exclude := true
 	pts, err := CoordAll(c, &exclude)
-	assert.Nil(t, err, "coord all err")
+	if err != nil {
+		t.Errorf("CoordAll error %v", err)
+	}
 
 	assert.Equal(t, len(pts), 1)
-
 	assert.Equal(t, pts[0].Lat, 46.608058)
 	assert.Equal(t, pts[0].Lng, -112.0372)
 }
