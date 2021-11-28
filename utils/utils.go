9e36bb6eb683a821d8096fdbce689e37d1aed4f8
Thomas Chavakis <tomchavakis@gmail.com>
2021-11-28 14:33:41 +0100

    Add IsArray function

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"reflect"
+)
 
 // LoadJSONFixture loads a testing file
 func LoadJSONFixture(filename string) (string, error) {
@@ -12,4 +15,10 @@ func LoadJSONFixture(filename string) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+// IsArray returns true of the interface is an array.
+func IsArray(v interface{}) bool {
+	rv := reflect.ValueOf(v)
+	return rv.Kind() == reflect.Array || rv.Kind() == reflect.Slice
 }
